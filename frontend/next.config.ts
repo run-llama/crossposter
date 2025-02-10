c45555af530f0c87657d4e45cfe137128f0305b1
Laurie Voss <github@seldo.com>
2025-02-10 10:40:28 -0800

    Privacy policy and ToS as HTML for Google

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 import withLlamaIndex from "llamaindex/next";
+import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default withLlamaIndex(nextConfig);
+const withMDX = createMDX({})
+
+export default withMDX(withLlamaIndex(nextConfig));
